ac6ebe75e5bce9f512f53635824ae301ab1054e1
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2019-10-09 12:46:54 +0200

    Ejercicios de clase 09-10

---
+++ Ejercicios_De_Clases/Codificación_ISO8859_UFT8/src/codificación_iso8859_uft8/Codificación_ISO8859_UFT8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codificación_iso8859_uft8;
+
+/**
+ *
+ * @author douglas
+ */
+public class Codificación_ISO8859_UFT8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Ejercicios_De_Clases/InsertarLineaEnFichero/src/insertarlineaenfichero/InsertarLineaEnFichero.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insertarlineaenfichero;
+
+import com.sun.source.tree.Tree;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.print.DocFlavor;
+
+/**
+ *
+ * @author douglas
+ */
+public class InsertarLineaEnFichero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nomFichero = args[0];
+        File fichOriginal = new File(nomFichero);
+                
+        if(args.length < 1)
+        {
+            System.out.println("Indicar un fichero.");
+            return;
+        }
+        
+        if(!fichOriginal.exists())
+        {
+            System.out.println("El fichero no existe.");
+            return;
+        }
+        
+        try(BufferedReader bfr = new BufferedReader(new FileReader(fichOriginal)))
+        {
+            File fichTemp = File.createTempFile(nomFichero, "");
+            BufferedWriter bfw = new BufferedWriter(new FileWriter(fichTemp));
+            
+            String linea = bfr.readLine();
+            while(linea != null)
+            {
+                bfw.write(linea);
+                bfw.newLine();
+                bfw.write("----");
+                bfw.newLine();
+                linea = bfr.readLine();
+            }
+            bfw.close();
+            
+            String nombFicheroTemp = nomFichero + "." + new SimpleDateFormat("yyyyMMdd").format(new Date()) + ".bak";
+            
+            if(fichOriginal.renameTo(new File(nombFicheroTemp)))
+            {
+                System.out.println("Creado fichero backup.");
+                if(fichTemp.renameTo(new File(nomFichero)))
+                {
+                    System.out.println("Creado fichero nuevo.");
+                }
+            }
+            
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            
+        }
+    }
+    
+}
