6b00e69966173a94a240111a50ed01096be9533c
Douglas Warner Jurado Peña <douglaswarner.jp@gmail.com>
2020-01-29 12:44:39 +0100

    Empleado

---
+++ Tema 4 - Correspondencia Objeto Relacional/9-ORM_herencia_tabla_para_la_jerarquia/src/ORM/Revista.java
@@ -0,0 +1,23 @@
+package ORM;
+
+public class Revista extends Publicacion implements java.io.Serializable {
+
+  private String issn;
+
+  public Revista () {
+    }
+
+  public Revista(String nombre, String issn) {
+    super(nombre);
+    this.issn = issn;
+    }
+
+  public String getIssn() {
+    return this.issn;
+    }
+
+  public void setIssn(String issn) {
+    this.issn = issn;
+    }
+
+  }
---
+++ Tema 4 - Correspondencia Objeto Relacional/Empleado.sql
@@ -0,0 +1,9 @@
+create table empleado (
+    dni char(9) not null,
+    nom_emp varchar(255) not null,
+    
+    primary key (dni)
+);
+
+create table empleado_plantilla (
+    
